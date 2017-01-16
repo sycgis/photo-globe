dd8664aafd12ba88eaf92b3fdd9bad1dde6f0a95
hwstovall <hwstovall@gmail.com>
2017-01-15 20:19:58 -0500

    Got redux devtools working.

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,14 @@ import App from "./containers/App";
 import { createStore } from "redux";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+declare const window;
+declare const module;
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const history = syncHistoryWithStore(browserHistory, store);
 
 render();
 
-declare const module;
 
 if (module.hot) {
   module.hot.accept('./containers/App', () => {
